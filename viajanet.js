98126bc29efcd8cdd47780ee5188df41c3a6bc9f
Fabiano Bandeira <lfabianosb@gmail.com>
2017-08-30 21:06:44 -0300

    Tratando valores nulos

--- viajanet.js
+++ viajanet.js
@@ -14,6 +14,12 @@ var ENCARGO = system.env.ENCARGO || 7;
 var TIMEOUT = system.env.SCRAP_TIMEOUT || 45;
 var flight = {};
 
+function throwErrorAndStop(msg) {
+    flight.msg = msg;
+    console.log(JSON.stringify(flight, null, null));
+    this.exit();
+}
+
 casper.on('error', function(msg, backtrace) {
     flight.msg = 'Error message: ' + msg + '\nBacktrace: ' + backtrace;
     console.log(JSON.stringify(flight, null, null));
@@ -31,21 +37,30 @@ casper.start(target, function() {
             var totalPassagens = this.evaluate(function() {
                 return document.querySelector('#vn-content-view > div.fluxo-content > flight-detail > ul > li:nth-child(1) > div.price.sticky_price > div > p.ng-binding > span').textContent;
             });
+            if (totalPassagens == null) {
+                throwErrorAndStop('Valor das passagens não encontrado.');
+            }
             var fltTotalPassagens = totalPassagens.trim().substring(3).replace('.','').replace(',','.')
 
             var taxasEncargos = this.evaluate(function() {
                 return document.querySelector('#vn-content-view > div.fluxo-content > flight-detail > ul > li:nth-child(1) > div.price.sticky_price > div > p:nth-child(2) > span').textContent;
             });
+            if (taxasEncargos == null) {
+                throwErrorAndStop('Valor dos encargos não encontrado.');
+            }
             var fltTaxasEncargos = taxasEncargos.trim().substring(3).replace('.','').replace(',','.');
 
             var cia = this.evaluate(function() {
                 return document.querySelector('#vn-content-view > div.fluxo-content > flight-detail > ul > li:nth-child(1) > div.flights > ul.ng-scope.ida > li.flight.ng-scope.flight-ida > label > div.list-cias > div > span').textContent;
             });
+            if (cia == null) {
+                throwErrorAndStop('Companhia aérea não encontrada.');
+            }
 
             var total = parseFloat((1 - (ENCARGO/100)) * fltTotalPassagens) + parseFloat(fltTaxasEncargos);
             var fltTotal = total.toFixed(2);
 
-            flight.cia = cia;
+            flight.cia = cia.trim();
             flight.valor = fltTotal;
             console.log(JSON.stringify(flight, null, null));
         },
